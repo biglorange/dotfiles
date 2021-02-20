24dc35dd2c5834a66fd405468d031f15da611d55
biglorange <orange804072060@gmail.com>
2021-02-21 00:38:20 +0800

    init dotfiles

---
+++ dotinstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#xinitrc
+ln -s ~/OneDrive/OS/linux/dotfile/xinitrc ~/.xinitrc
+ln -s ~/OneDrive/OS/linux/dotfile/xprofile ~/.xprofile
+ln -s ~/OneDrive/OS/linux/dotfile/zprofile ~/.zprofile
+ln -s ~/OneDrive/OS/linux/dotfile/zsh_history ~/.zsh_history
+
+# zshrc
+ln -s ~/OneDrive/OS/linux/dotfile/zshrc ~/.zshrc
+# i3 config
+ln -s ~/OneDrive/OS/linux/dotfile/i3/config ~/.config/i3/config
+# i3status-rust
+ln -s ~/OneDrive/OS/linux/dotfile/i3/i3status_config ~/.config/i3status-rs/config.toml
