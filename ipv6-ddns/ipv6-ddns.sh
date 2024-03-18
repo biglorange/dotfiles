606ee5cf124e376f2a65cc5d221ce91f5d631c18
biglorange <orange804072060@gmail.com>
2024-03-18 23:27:21 +0800

    fix: update ipv6-ddns.sh
    1. 添加新旧字段属性判断,相同则不进行更新
    2. 添加按时间添加开启 proxy 属性,工作日 8:00-18:00 开启代理

--- ipv6-ddns/ipv6-ddns.sh
+++ ipv6-ddns/ipv6-ddns.sh
@@ -16,6 +16,22 @@ ROOT_DIR=$(dirname $(readlink -f "$0"))
 # curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records/${DNS_ID}" \
 #     -H "X-Auth-Email: ${EMAIL}" -H "X-Auth-Key: ${AUTH_KEY}" -H "Content-Type: application/json" \
 #     --data '{"type":"'"${DNS_TYPE}"'","name":"'"${HOST_NAME}"'","content":"'"${IP6}"'","ttl":120,"proxied":false}'
+# https://cdn.jsdelivr.net/gh/NateScarlet/holiday-cn@master/{年份}.json
+# 可以获取法定节假日进行判断
+get_proxy_flag()
+{
+    current_hour=$(date +%H)
+    current_date=$(date +%u)
+
+    proxy_flag="false"
+
+    if [[ ${current_date} -ge 1 && ${current_date} -le 5 && ${current_hour} -ge 8 && ${current_hour} -le 18 ]]; then
+        proxy_flag="true"
+    fi
+
+    echo "${proxy_flag}"
+}
+
 
 
 ipv6_ddns_set()
@@ -24,13 +40,21 @@ ipv6_ddns_set()
     config_file=$1
     . ${ROOT_DIR}/${config_file}
 
+    PROXY="$(get_proxy_flag)"
+
+    tmp_result=$(curl -X GET "https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records/${DNS_ID}" \
+        -H "X-Auth-Email: ${EMAIL}" -H "X-Auth-Key: ${AUTH_KEY}" -H "Content-Type: application/json")
+
+    old_ipv6_address="$(echo "${tmp_result}" | grep -oE '"content":"[0-9a-fA-F:]*"' | awk -F'"' '{print $4}')"
+    old_proxy_flag="$(echo "${tmp_result}" | grep -oE '"proxied":(false)|(true)' | awk -F':' '{print $2}' | tr -d '\n')"
+
     IP6=`ip -6 addr show dev ${ETH_CARD} | grep global | awk '{print $2}' | awk -F "/" '{print $1}' | head -1`
     [ -z ${IP6} ] && exit
     # ipv6 地址以 fe 开头则是本地地址
-    [[ "${IP6}" == fe* ]] && exit
+    [[ "${IP6}" == fe* || "${old_ipv6_address}" == "${IP6}" && ${old_proxy_flag} == "${PROXY}" ]] && exit
     curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records/${DNS_ID}" \
         -H "X-Auth-Email: ${EMAIL}" -H "X-Auth-Key: ${AUTH_KEY}" -H "Content-Type: application/json" \
-        --data '{"type":"'"${DNS_TYPE}"'","name":"'"${HOST_NAME}"'","content":"'"${IP6}"'","ttl":120,"proxied":false}'
+        --data '{"type":"'"${DNS_TYPE}"'","name":"'"${HOST_NAME}"'","content":"'"${IP6}"'","ttl":120,"proxied":'"${PROXY}"'}'
 }
 
 
