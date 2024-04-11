fbd5273e73775333841ffa4b22751ddeeeada0eb
biglorange <orange804072060@gmail.com>
2024-04-11 22:55:27 +0800

    update: update dotinstall.sh
    调整 mkdir 命令和 ln 命令使用,使用函数形式统一进行文件夹创建和软链接

--- dotinstall.sh
+++ dotinstall.sh
@@ -1,80 +1,86 @@
 #!/bin/bash
 
-current_dir=`pwd`
-#xinitrc
-rm -f ~/.xinitrc
-ln -s ${current_dir}/xinitrc ~/.xinitrc
-rm -f ~/.xprofile
-ln -s ${current_dir}/xprofile ~/.xprofile
-rm -f ~/.zprofile
-ln -s ${current_dir}/zprofile ~/.zprofile
+function link_file_after_delete()
+{
+    file_path=$1
+    link_file=$2
 
+    if [ -f "link_file" ]; then
+        rm -f "link_file"
+    fi
+
+    if [ -f "$file_path" ]; then
+        ln -s "$file_path" "$link_file"
+    fi
+}
+
+function chk_path_mk()
+{
+    file_path=$1
+    if [ ! -d "$file_path" ]; then
+        mkdir -p "$file_path"
+    fi
+}
+
+current_dir=$(pwd)
+#xinitrc
+link_file_after_delete "${current_dir}/xinitrc" "${HOME}/.xinitrc"
+link_file_after_delete "${current_dir}/xprofile" "${HOME}/.xprofile"
+link_file_after_delete "${current_dir}/zprofile" "${HOME}/.zprofile"
 
 # zshrc
-rm -f ~/.zshrc
-ln -s ${current_dir}/zshrc ~/.zshrc
-ln -s ${current_dir}/zshrc_ext ~/.zshrc_ext
-# rm -f ~/.zsh_history
-# ln -s ${current_dir}/zsh_history ~/.zsh_history
+link_file_after_delete "${current_dir}/zshrc" "${HOME}/.zshrc"
+link_file_after_delete "${current_dir}/zshrc_ext" "${HOME}/.zshrc_ext"
+
 # i3 config
-[ ! -d ~/.config ] && mkdir -p ~/.config
-[ ! -d ~/.config/i3 ] && mkdir -p ~/.config/i3
-rm -f ~/.config/i3/config
-ln -s ${current_dir}/i3/config ~/.config/i3/config
+chk_path_mk "${HOME}/.config/i3"
+link_file_after_delete "${current_dir}/i3/config" "${HOME}/.config/i3/config"
+
 # i3status-rust
-[ ! -d ~/.config/i3status-rs ] && mkdir -p ~/.config/i3status-rs
-rm -f ~/.config/i3status-rs/config.toml
-ln -s ${current_dir}/i3/i3status_config ~/.config/i3status-rs/config.toml
+chk_path_mk "${HOME}/.config/i3status-rs"
+link_file_after_delete "${current_dir}/i3/i3status_config" "${HOME}/.config/i3status-rs/config.toml"
+
 # dunst
-[ ! -d ~/.config/dunst ] && mkdir -p ~/.config/dunst
-rm -f ~/.config/dunst/dunstrc
-ln -s ${current_dir}/dunst/dunstrc ~/.config/dunst/dunstrc
+chk_path_mk "${HOME}/.config/dunst"
+link_file_after_delete "${current_dir}/dunst/dunstrc" "${HOME}/.config/dunst/dunstrc"
+
 # gitcofnig
-rm -f ~/.gitconfig
-rm -f ~/.gitignore
-ln -s ${current_dir}/gitconfig ~/.gitconfig
-ln -s ${current_dir}/gitignore ~/.gitignore
-ln -s ${current_dir}/git/gitconfig_ext ~/.gitconfig_ext
+link_file_after_delete "${current_dir}/gitconfig" "${HOME}/.gitconfig"
+link_file_after_delete "${current_dir}/gitignore" "${HOME}/.gitignore"
+link_file_after_delete "${current_dir}/git/gitconfig_ext" "${HOME}/.gitconfig_ext"
 # aria2
-[ ! -d ~/.config/.aria2 ] && mkdir -p ~/.config/.aria2
-rm -f ~/.aria2/aria2.conf
-ln -s ${current_dir}/aria2/aria2.conf ~/.aria2/aria2.conf
-touch ~/.aria2/aria2.session
+chk_path_mk "${HOME}/.config/.aria2"
+link_file_after_delete ${current_dir}/aria2/aria2.conf ${HOME}/.aria2/aria2.conf
+touch "${HOME}/.aria2/aria2.session"
+
 # onedrive
-[ ! -d ~/.config/onedrive ] && mkdir -p ~/.config/onedrive
-rm -f ~/.config/onedrive/config
-ln -s ${current_dir}/onedrive/onedrive_personal/config ~/.config/onedrive/config
-# ln -s ${current_dir}/onedrive/onedrive_edu/config ~/.config/onedrive_edu/config
+chk_path_mk "${HOME}/.config/onedrive"
+link_file_after_delete "${current_dir}/onedrive/onedrive_personal/config" "${HOME}/.config/onedrive/config"
+# link_file_after_delete ${current_dir}/onedrive/onedrive_edu/config ${HOME}/.config/onedrive_edu/config
 # fiqlo
-rm -f ~/.xscreensaver
-ln -s ${current_dir}/xscreensaver ~/.xscreensaver
+link_file_after_delete "${current_dir}/xscreensaver" "${HOME}/.xscreensaver"
 # mpv
-rm -rf ~/.config/mpv
-ln -s ${current_dir}/mpv ~/.config/
+rm -rf "${HOME}/.config/mpv"
+link_file_after_delete "${current_dir}/mpv" "${HOME}/.config/"
 # xmodmap
-rm -f ~/.Xmodmap
-ln -s ${current_dir}/Xmodmap ~/.Xmodmap
+link_file_after_delete "${current_dir}/Xmodmap" "${HOME}/.Xmodmap"
 # compton/picom
-[ ! -d ~/.config/picom ] && mkdir -p ~/.config/picom
-rm -f ~/.config/picom/picom.conf
-ln -s ${current_dir}/i3/picom/picom.conf ~/.config/picom/picom.conf
+chk_path_mk "${HOME}/.config/picom"
+link_file_after_delete "${current_dir}/i3/picom/picom.conf" "${HOME}/.config/picom/picom.conf"
 # paru
-[ ! -d ~/.config/paru ] && mkdir -p ~/.config/paru
-rm -f ~/.config/paru/paru.conf
-ln -s ${current_dir}/paru/paru.conf ~/.config/paru/paru.conf
+chk_path_mk "${HOME}/.config/paru"
+link_file_after_delete "${current_dir}/paru/paru.conf" "${HOME}/.config/paru/paru.conf"
 # cargo config
-[ ! -d ~/.cargo ] && mkdir -p ~/.cargo
-rm -f ~/.cargo/config
-ln -s ${current_dir}/cargo/cargo.conf ~/.cargo/config
+chk_path_mk "${HOME}/.cargo"
+link_file_after_delete "${current_dir}/cargo/cargo.conf" "${HOME}/.cargo/config"
 
 sudo rm -f /etc/pacman.conf
-sudo ln -s ${current_dir}/pacman/pacman.conf /etc/pacman.conf
+sudo ln -s "${current_dir}/pacman/pacman.conf" "/etc/pacman.conf"
 
 sudo rm -f /etc/pacman.d/mirrorlist
-sudo ln -s ${current_dir}/pacman/pacman.d/mirrorlist /etc/pacman.d/mirrorlist
+sudo ln -s "${current_dir}/pacman/pacman.d/mirrorlist" "/etc/pacman.d/mirrorlist"
 
 sudo rm -f /etc/pacman.d/hooks
-sudo ln -s ${current_dir}/pacman/pacman.d/hooks /etc/pacman.d/hooks
+sudo ln -s "${current_dir}/pacman/pacman.d/hooks" "/etc/pacman.d/hooks"
 
-rm -f ~/.npmrc
-ln -s ${current_dir}/npm/npmrc ~/.npmrc
+link_file_after_delete "${current_dir}/npm/npmrc" "${HOME}/.npmrc"
